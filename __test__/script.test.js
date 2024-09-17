5fa79da7234e841f23ee6686526aa3fe06f37f63
Horvath Mark <brryy0371@gmail.com>
2024-09-17 20:11:06 +0200

    elnezest

--- __test__/script.test.js
+++ __test__/script.test.js
@@ -1,19 +1,17 @@
-module.exports = function Osztok(szam){
+const Osztok = require('../script.js')
 
-    if (typeof szam === 'string'){
-        throw new Error("Nem számot adtál meg!")
-    }
+test("Ha sztringet adnak meg, akkor hibát kellene dobni", () => {
+    expect(()=>Osztok('Szia!')).toThrow('Nem számot adtál meg!')
+})
 
-    if (!Number.isInteger(szam)){
-        throw new Error("Nem egész számot adtál meg!")
-    }
+test("Tört számnál hibát ír ki.", () =>{
+    expect(() => Osztok(3.5)).toThrow('Nem egész számot adtál meg')
+})
 
-    let osszeg = 0;
-    for(let i = 1; i <= szam; i++){
-        if (szam % i === 0){
-            osszeg += i;
-        }
-    }
-    return osszeg;
+test("6 osztóinak összege 12-nek kell lennie.", () =>{
+    expect(Osztok(6)).toBe(12)
+})
 
-}
+test("6 osztóinak összege nem 13.", () =>{
+    expect(Osztok(6)).toBe(12)
+})
--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const Osztok = require('../script.js')
+module.exports = function Osztok(szam){
 
-test("Ha sztringet adnak meg, akkor hibát kellene dobni", () => {
-    expect(()=>Osztok('Szia!')).toThrow('Nem számot adtál meg!')
-})
+    if (typeof szam === 'string'){
+        throw new Error("Nem számot adtál meg!")
+    }
 
-test("Tört számnál hibát ír ki.", () =>{
-    expect(() => Osztok(3.5)).toThrow('Nem egész számot adtál meg')
-})
+    if (!Number.isInteger(szam)){
+        throw new Error("Nem egész számot adtál meg!")
+    }
 
-test("6 osztóinak összege 12-nek kell lennie.", () =>{
-    expect(Osztok(6)).toBe(12)
-})
+    let osszeg = 0;
+    for(let i = 1; i <= szam; i++){
+        if (szam % i === 0){
+            osszeg += i;
+        }
+    }
+    return osszeg;
 
-test("6 osztóinak összege nem 13.", () =>{
-    expect(Osztok(6)).toBe(12)
-})
+}
